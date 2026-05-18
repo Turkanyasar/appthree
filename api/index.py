634999faba24e5bae12495b88c68ee2b6be8a745
Turkanyasar <turkanaliyevayasar@gmail.com>
2026-05-18 11:41:11 +0000

    commit

---
+++ api/index.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+from flask_cors import CORS 
+from .routers import get_news
+
+app = Flask(__name__) 
+
+CORS(app resources={r"/api/news":{
+    "origins": []
+    "methods": ["GET"]
+}})
+
+@app.route("/api/news", methods=["GET"])
+def news_route():
+    return get_news()
